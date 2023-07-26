b8e354a6ca957c399bd8e3cf3c21ee682800cac0
Nikita <nikita.ushackow.02@gmail.com>
2023-07-26 15:00:21 +0200

    dev: start setting typescript and tsx file in webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,69 +1,34 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const paths = require('./paths')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  // Where webpack looks to start building the bundle
-  entry: [paths.src + '/index.js'],
-
-  // Where webpack outputs the assets and bundles
-  output: {
-    path: paths.build,
-    filename: '[name].bundle.js',
-    publicPath: '/',
-  },
-
-  // Customize the webpack build process
-  plugins: [
-    // Removes/cleans build folders and unused assets when rebuilding
-    new CleanWebpackPlugin(),
-
-    // Copies files from target to destination folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-
-    // Generates an HTML file from a template
-    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
-    new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      favicon: paths.src + '/images/favicon.png',
-      template: paths.src + '/template.html', // template file
-      filename: 'index.html', // output file
-    }),
-  ],
-
-  // Determine how modules within the project are treated
-  module: {
-    rules: [
-      // JavaScript: Use Babel to transpile JavaScript files
-      { test: /\.js$/, use: ['babel-loader'] },
-
-      // Images: Copy image files to build folder
-      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
-
-      // Fonts and SVGs: Inline files
-      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
-    ],
-  },
-
-  resolve: {
-    modules: [paths.src, 'node_modules'],
-    extensions: ['.js', '.jsx', '.json'],
-    alias: {
-      '@': paths.src,
-      assets: paths.public,
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[fullhash].bundle.js',
     },
-  },
-}
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/, // Расширения файлов, которые должны обрабатываться TypeScript и Babel
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'], // Подключаем Babel пресеты для React и TypeScript
+                    },
+                },
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(), // Очищаем папку сборки перед каждой новой сборкой
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // Исходный HTML файл для генерации нового с подключением собранных скриптов
+        }),
+    ],
+};
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,38 +1,25 @@
-const { merge } = require('webpack-merge')
+const path = require("path");
+const {merge} = require("webpack-merge");
 
-const common = require('./webpack.common')
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  // Set the mode to development or production
-  mode: 'development',
-
-  // Control how source maps are generated
-  devtool: 'inline-source-map',
-
-  // Spin up a server for quick development
-  devServer: {
-    historyApiFallback: true,
-    open: true,
-    compress: true,
-    hot: true,
-    port: 8080,
-  },
-
-  module: {
-    rules: [
-      // Styles: Inject CSS into the head with source maps
-      {
-        test: /\.(sass|scss|css)$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true, importLoaders: 1, modules: false },
-          },
-          { loader: 'postcss-loader', options: { sourceMap: true } },
-          { loader: 'sass-loader', options: { sourceMap: true } },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'), // Корневая директория для dev-сервера
+        port: 3000, // Порт для запуска dev-сервера
+        hot: true,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // Обработка CSS файлов
+                use: ['style-loader', 'css-loader'],
+            },
         ],
-      },
-    ],
-  },
+    },
+    plugins: [],
 })
