2bf12cb9c0968e93ecb39368de1348a85387f9c8
Nikita <nikita.ushackow.02@gmail.com>
2023-07-26 15:02:07 +0200

    dev: add files ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum officia optio quisquam repellat temporibus veniam? Aliquam ipsam ipsum tempore veniam.
+        </div>
+    );
+};
+
+export default App;
