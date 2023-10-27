c66a5682531eddcbaa3ed7f02c2fff271eba095d
Nikita <nikita.ushackow.02@gmail.com>
2023-10-27 18:01:28 +0200

    dev: npm run dev - change in realtime

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className={"hello"}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum officia optio quisquam repellat temporibus veniam? Aliquam ipsam ipsum tempore veniam.
+            Lorem ipsum dolor sit amet
         </div>
     );
 };
